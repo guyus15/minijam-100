7d044bd02bb9160e090083e7fd4309a5ffdd8731
Guy Chamberlain-Webber <chamberlainwebberj@gmail.com>
2022-02-18 10:48:31 +0000

    Created the main file which will act as the entry point for the program

---
+++ main.py
@@ -0,0 +1,28 @@
+# Simple pygame program
+
+# Import and initialize the pygame library
+import pygame
+
+pygame.init()
+
+size = width, height = 800, 500
+
+# Set up the drawing window
+screen = pygame.display.set_mode(size)
+
+# Run until the user asks to quit
+running = True
+while running:
+
+    # Checking for the user pressing the window close button
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+
+    pygame.display.set_caption("Mini Jam Game")
+
+    # Flip the display
+    pygame.display.flip()
+
+# Quit the game
+pygame.quit()
