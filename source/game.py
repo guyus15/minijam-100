2787bfe9acf90c8a8746e3e279d20ee27f516a75
Guy Chamberlain-Webber <chamberlainwebberj@gmail.com>
2022-02-20 22:34:08 +0000

    Replaced single trash spawner with an array of multiple trash spawners

--- source/game.py
+++ source/game.py
@@ -16,7 +16,8 @@ class Game:
         self.clock = pygame.time.Clock()
         self.player = Player((SCREEN_WIDTH / 2 - 32, SCREEN_HEIGHT / 2 - 32))
         self.incinerator = Incinerator(Vector(20, 0), Vector(100, 100), self.player)
-        self.trash_spawner = TrashSpawner(Vector(SCREEN_WIDTH / 2, SCREEN_HEIGHT - 100), (0, -1), self.player)
+        self.trash_spawners = [TrashSpawner(Vector(SCREEN_WIDTH / 2, SCREEN_HEIGHT - 100), (0, -1), self.player),
+                               TrashSpawner(Vector(SCREEN_WIDTH / 2, 100), (0, 1), self.player)]
         self.level = Level()
         self.background_image = pygame.Surface.convert(pygame.image.load(BACKGROUND_IMAGE_PATH))
 
@@ -29,7 +30,8 @@ class Game:
         self.screen.blit(self.background_image, (0, 0))
 
         self.incinerator.update(self.screen)  # Updates incinerator behaviour
-        self.trash_spawner.update(self.screen)  # Updates trash spawner behaviour
+        for trash_spawner in self.trash_spawners:
+            trash_spawner.update(self.screen)  # Updates trash spawner behaviour
         self.player.update(self.screen)  # Updates player behaviour
 
         TimeManager.tick()
