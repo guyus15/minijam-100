9528b8e35e19a07f94e996b48e0fe325b7be971a
Guy Chamberlain-Webber <chamberlainwebberj@gmail.com>
2022-02-20 18:27:28 +0000

    Added a TimeManager class which will keep record of have many ticks the game has completed

---
+++ source/time_manager.py
@@ -0,0 +1,15 @@
+class TimeManager:
+    time = 0  # The initial clock time
+
+    @classmethod
+    def tick(cls):
+        cls.time += 1
+
+    @classmethod
+    def transition(cls, frame_duration):
+        # Used to check if a sufficient amount of time has passed
+
+        if cls.time & frame_duration == 0:
+            return True
+
+        return False
